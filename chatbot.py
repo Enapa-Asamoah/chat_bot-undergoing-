c032df96f920d22aa80538168dc69029e975cda1
brabentil <128573509+brabentil@users.noreply.github.com>
2024-06-16 17:34:37 +0000

    Integrate fuzzy matching for intent recognition

--- chatbot.py
+++ chatbot.py
@@ -2,14 +2,10 @@ import random
 import json
 import pickle
 import numpy as np
-
 import nltk
 from nltk.stem import WordNetLemmatizer
-
+from fuzzywuzzy import process
 import tensorflow as tf
-
-
-
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
@@ -20,7 +16,7 @@ model = tf.keras.models.load_model('chatbot_model.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
 def bag_of_words(sentence):
@@ -37,7 +33,6 @@ def predict_class(sentence):
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -53,12 +48,25 @@ def get_response(intents_list):
             break
     return result
 
+def match_intent(sentence, intents):
+    all_patterns = [pattern for intent in intents['intents'] for pattern in intent['patterns']]
+    best_match = process.extractOne(sentence, all_patterns)
+    return best_match
+
 print("GO! Bot is running!")
 
 while True:
     print("You: ")
     message = input("")
-    ints = predict_class(message)
-    res = get_response(ints)
-    print("Chat: ")
-    print(res)
+    match = match_intent(message, intents)
+    if match and match[1] > 80:  # Match threshold
+        matched_pattern = match[0]
+        for intent in intents['intents']:
+            if matched_pattern in intent['patterns']:
+                ints = [{'intent': intent['tag'], 'probability': '1.0'}]
+                break
+        res = get_response(ints)
+        print("Chat: ")
+        print(res)
+    else:
+        print("Chat: I didn't understand that.")
